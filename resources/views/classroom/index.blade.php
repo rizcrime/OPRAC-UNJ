3555475710a2e5f49c2b3b8453947de538c3189f
rizcrime <rizkiafh180@gmail.com>
2022-03-31 16:35:59 +0700

    classroom 80% is done -revision

--- resources/views/classroom/index.blade.php
+++ resources/views/classroom/index.blade.php
@@ -1,25 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<<<<<<< Updated upstream
-    <div class="container">
-        <div class="row" style="justify-content: center; margin: 2%;">
-            @foreach($data as $item)
-            <div class="col-sm-6 card" style="width: 18rem; margin: 2%;">
-                <img src="{{ asset('media/logo-google-meet.png') }}" class="card-img-top">
-                <div class="card-body">
-                    <a class="card-text" href="{{ $item->link }}">{{ $item->users_name }}</a>
-                </div>
-            </div>
-            @endforeach
-            @if(Auth::user()->role != 'Mahasiswa')
-            <div id="create-class" class="col-sm-6 card" style="width: 18rem; margin: 2%;">
-                <img src="{{ asset('media/add.png') }}" class="card-img-top">
-                <div class="card-body">
-                    <a class="card-text" href="#">Tambah Kelas</a>
-                </div>
-            </div>
-            @endif
-=======
 <div class="container">
     <div class="row" style="justify-content: center; margin: 2%;">
         @foreach($classroom as $item)
@@ -97,7 +77,6 @@
                     </div>
                 </form>
             </div>
->>>>>>> Stashed changes
         </div>
     </div>
 @endsection
